b1f3fc7f88f14ff6ab0becc48eff5dd6b6e2b8da
shri234 <43914954+shri234@users.noreply.github.com>
2025-10-29 18:43:15 +0530

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -38,14 +38,14 @@ export class AuthController {
       res.cookie('jwt', result.accessToken, {
         httpOnly: true,
         secure: false,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
       });
 
       res.cookie('refreshToken', result.refreshToken, {
         httpOnly: true,
         secure: false,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
